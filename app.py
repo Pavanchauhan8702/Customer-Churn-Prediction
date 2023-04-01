328a22e163bf70869afe1cd4de0d74ef60e72658
Pavan Chauhan <pavanchauhan449@gmail.com>
2023-04-01 22:46:38 +0530

    apps commit

--- app.py
+++ app.py
@@ -1,85 +1,54 @@
-import streamlit as st
-import pandas as pd
+import pickle
+from flask import Flask, jsonify, request,app,url_for,render_template
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
-# Define a function to preprocess the data
-def preprocess_data(df):
-    # Replace missing values with mean
-    df.fillna(df.mean(), inplace=True)
-    # Convert categorical variables to numerical using one-hot encoding
-    df = pd.get_dummies(df, columns=['gender', 'education', 'marital_status', 'payment_method'])
-    return df
+app = Flask(__name__)
+#load the model
+model =pickle.load(open('lr_model.pkl','rb'))
 
-# Load the data
-df = pd.read_csv('train.csv')
+# Define API endpoints
+@app.route('/')
+def home():
+ return render_template('home.html')
 
-# Preprocess the data
-df = preprocess_data(df)
+# Define API endpoints
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    print(data)
+    gender = data['gender']
+    SeniorCitizen = data['SeniorCitizen']
+    Partner = data['Partner']
+    Dependents = data['Dependents']
+    tenure = data['tenure']
+    PhoneService = data['PhoneService']
+    MultipleLines = data['MultipleLines']
+    InternetService = data['InternetService']
+    OnlineSecurity = data['OnlineSecurity']
+    OnlineBackup = data['OnlineBackup']
+    DeviceProtection = data['DeviceProtection']
+    TechSupport = data['TechSupport']
+    StreamingTV = data['StreamingTV']
+    StreamingMovies = data['StreamingMovies']
+    Contract = data['Contract']
+    PaperlessBilling = data['PaperlessBilling']
+    PaymentMethod = data['PaymentMethod']
+    MonthlyCharges = data['MonthlyCharges']
+    TotalCharges = data['TotalCharges']
 
-# Split the data into X and y
-X = df.drop(['churn'], axis=1)
-y = df['churn']
+ # Preprocess input data
+    input_data = pd.DataFrame({'gender': [gender], 'SeniorCitizen': [SeniorCitizen], 'Partner': [Partner], 'Dependents': [Dependents], 'tenure': [tenure], 'PhoneService': [PhoneService], 'MultipleLines': [MultipleLines], 'InternetService': [InternetService], 'OnlineSecurity': [OnlineSecurity], 'OnlineBackup': [OnlineBackup], 'DeviceProtection': [DeviceProtection], 'TechSupport': [TechSupport], 'StreamingTV': [StreamingTV], 'StreamingMovies': [StreamingMovies], 'Contract': [Contract], 'PaperlessBilling': [PaperlessBilling], 'PaymentMethod': [PaymentMethod], 'MonthlyCharges': [MonthlyCharges], 'TotalCharges': [TotalCharges]})
+    input_data['TotalCharges'] = pd.to_numeric(input_data['TotalCharges'], errors='coerce')
+    input_data = imputer.transform(input_data)
+    input_data = scaler.transform(input_data)
 
-# Scale the data
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+ # Make predictions
+    lr_pred = lr.predict(input_data)[0]
 
-# Train a Logistic Regression model
-logreg = LogisticRegression()
-logreg.fit(X_scaled, y)
+# Return results
+    result = {'Logistic Regression': lr_pred}
+    return jsonify(result)
 
-# Define a function to make predictions using the model
-def predict_churn(gender, age, education, marital_status, income, payment_method, monthly_charge, data_usage, total_calls_made):
-    # Convert input data to a dataframe
-    input_data = pd.DataFrame({'gender': gender,
-                                'age': age,
-                                'education': education,
-                                'marital_status': marital_status,
-                                'income': income,
-                                'payment_method': payment_method,
-                                'monthly_charge': monthly_charge,
-                                'data_usage': data_usage,
-                                'total_calls_made': total_calls_made}, index=[0])
-
-    # Preprocess the input data
-    input_data = preprocess_data(input_data)
-
-    # Scale the input data
-    input_data_scaled = scaler.transform(input_data)
-
-    # Make a prediction using the trained model
-    prediction = logreg.predict(input_data_scaled)[0]
-
-    return prediction
-
-# Create the Streamlit app
-def main():
-    st.title('Telecom Customer Churn Prediction')
-
-    # Add input fields for user to enter data
-    gender = st.selectbox('Gender', ['Male', 'Female'])
-    age = st.slider('Age', 18, 100)
-    education = st.selectbox('Education', ['High School or Below', 'College', 'Bachelor', 'Master or Above'])
-    marital_status = st.selectbox('Marital Status', ['Single', 'Married', 'Divorced'])
-    income = st.slider('Income', 0, 100000, 50000)
-    payment_method = st.selectbox('Payment Method', ['Credit Card', 'Bank Transfer', 'Mailed Check'])
-    monthly_charge = st.slider('Monthly Charge', 0, 500, 100)
-    data_usage = st.slider('Data Usage', 0, 50, 10)
-    total_calls_made = st.slider('Total Calls Made', 0, 20, 5)
-
-    # Make a prediction using the predict_churn function
-    prediction = predict_churn(gender, age, education, marital_status, income, payment_method, monthly_charge, data_usage, total_calls_made)
-
-    # Display the prediction
-    st.subheader('Prediction')
-    if prediction == 0:
-        st.write('Customer is not likely to churn.')
-    else:
-        st.write('Customer is likely to churn.')
-
-# Run the Streamlit app
-if _name_== '_main_':
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
